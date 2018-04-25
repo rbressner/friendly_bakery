264be106ac42056f9b16d60a15745409d2c907dc
Rachel Bressner <rachel.bressner@gmail.com>
2018-04-25 16:44:14 -0400

    css and such

--- app.rb
+++ app.rb
@@ -4,3 +4,19 @@ require 'sinatra'
 get "/" do
 erb :index
 end
+
+get "/muffins" do
+  erb :muffins
+end
+
+get "/cookies" do
+  erb :cookies
+end
+
+get "/cake" do
+  erb :cake
+end
+
+get "/contact" do
+  erb :contact
+end
