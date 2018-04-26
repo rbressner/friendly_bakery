41c03748a1b252081e4a602b644ebf230639b479
Rachel Bressner <rachel.bressner@gmail.com>
2018-04-26 16:21:35 -0400

    the email is AMAZING' '

--- app.rb
+++ app.rb
@@ -27,7 +27,13 @@ post "/response" do
   from = Email.new(email: 'rachel.bressner@gmail.com')
   to = Email.new(email: params[:email])
   subject = 'hi ' + params[:name]
-  content = Content.new(type: 'text/html', value: "<html><body><h1>Thanks for your interest in our baked goods!</h1><br> <img src='https://preview.ibb.co/nOvD4H/catalogue.jpg' alt='catalogue' border='0'> <br><h2>With love, <br> Your friends at Mary Jane's</h2></body></html>")
+  content = Content.new(type: 'text/html', value:
+    "<html><body style='background-color: #e6ffe6; text-align: center;'>
+    <img src='https://image.ibb.co/nmKhgx/logo.png' alt='logo' border='0' style='display: block; margin-right: auto; margin-left: auto; top: 0; width: 25%;'>
+    <h1>Thanks for your interest in our baked goods!</h1><br>
+    <img src='https://preview.ibb.co/nOvD4H/catalogue.jpg' alt='catalogue' border='0' style='  display: block; margin-left: auto; margin-right: auto;'>
+    <br><h2>With love, <br> Your friends at Mary Jane's</h2>
+    </body></html>")
   mail = Mail.new(from, subject, to, content)
 
   sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
