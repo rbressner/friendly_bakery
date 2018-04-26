af63e29bb04c14c600c60f0313e3e93467d87efd
Rachel Bressner <rachel.bressner@gmail.com>
2018-04-26 13:00:43 -0400

    I got the fjkawljhflh img in the email YASSSS

--- app.rb
+++ app.rb
@@ -27,7 +27,7 @@ post "/response" do
   from = Email.new(email: 'rachel.bressner@gmail.com')
   to = Email.new(email: params[:email])
   subject = 'hi ' + params[:name]
-  content = Content.new(type: 'text/plain', value: 'hi there')
+  content = Content.new(type: 'text/html', value: "<html><body><h1>Thanks for your interest in our baked goods!</h1><br> <img src='https://preview.ibb.co/nOvD4H/catalogue.jpg' alt='catalogue' border='0'> <br><h2>With love, <br> Your friends at Mary Jane's</h2></body></html>")
   mail = Mail.new(from, subject, to, content)
 
   sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
